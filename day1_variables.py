0099f3898d173abc904d6c821064b618f3b9473e
kavana yelshetty <kacy29062007@gmail.com>
2026-03-24 21:29:59 +0530

    update file

--- day1_variables.py
+++ day1_variables.py
@@ -2,3 +2,13 @@ name ="kavana"
 age =19
 print("My name is",name)
 print("My age is",age)
+x=10
+y=20
+print(type(x))
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
