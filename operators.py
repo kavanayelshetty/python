1c97110493db3a33446d0a960092234d6e176065
kavana yelshetty <kacy29062007@gmail.com>
2026-03-26 21:36:22 +0530

    file updated

--- operators.py
+++ operators.py
@@ -9,3 +9,9 @@ print(a>=b)
 print(a>10 and b>10)
 print(a>10 or b>10)
 print(not(a>15))
+list=[1,2,3,4,5]
+str="python"
+print(3 in list)
+print(6 not in list)
+print("p" in str)
+print("z" not in str)
