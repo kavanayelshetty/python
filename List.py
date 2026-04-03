2e3994727198ef731aaf29984af9ad5004fd03eb
kavana yelshetty <kacy29062007@gmail.com>
2026-04-03 19:38:09 +0530

    file update

--- List.py
+++ List.py
@@ -1,47 +1,8 @@
-t.l =["apple","banana","mango"]
-print (l)
-numbers=[1,2,3,4,5]
-mixed=["apple",1,"banana",2,4]
-print(numbers)
-print(l(0))
-l[1]="cherry" 
-print(l)
-l.append("tomato")
-print(l)
-a=["America","rome","France"]
-print.max(a)
-print(lower(a))
-print(upper(a))
-print.min(a)
-s=["tirupati","gulbarga","banglore"]
-print.max(s)
-print.min(s)
-my_dict={
-      " key 1": "value 1"
-      " key 2": "value 2"
-      " key 3": "value 3"
-}
-print.upper(s)
-print.lower(s)
-s=append.("mysore")
-t2=(4,5,6)
-combined_t=t1+t2
-print(combined_t)
-t1.append(9)
-print(t1)
-pop.(t1)
-pop.(t1[1])
-pop.(t1[2])
-reapeted_tuple=(1,2)*3
-print(repeated_tuple)
-Karnataka_food={
-     "banglore ":" mudde "
-     "mysore ":"vade"
-     " Gulbarga ":" roti "
-}
-print("Karnataka_food"["mysore"])
-pint("Karnataka_food".get["mysore"])
-print("Karnataka_food".get["shivamogga","not found"])
+fruits = ["apple","banana","cherry"]
+print(fruits)
+print(fruits[1])
+fruits[2]="mango"
+print(fruits)
 
 
 
