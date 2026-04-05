b8afeb07ddde86a01880bccc2e13cba1c6cebbc3
kavana yelshetty <kacy29062007@gmail.com>
2026-04-05 20:18:17 +0530

    file updated

--- set.py
+++ set.py
@@ -4,3 +4,9 @@ number={1,2,3,4,5}
 number2={3,8,9,7,6}
 union_set= fruits| number|number2
 print(union_set)
+set=number-number2
+print(set)
+set2=number^number2
+print(set2)
+fruits.remove("banana")
+print(fruits)
