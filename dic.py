4b9cc302133edf82ba1418a79ba176dc2653f09b
kavana yelshetty <kacy29062007@gmail.com>
2026-04-06 21:22:52 +0530

    file updated

--- dic.py
+++ dic.py
@@ -1,8 +1,12 @@
-food={
+friends={
     "kavana":"2007",
     "chandana" :"2005",
     "harshita":"2006"
 }
-print(food)
-print(food["harshita"])
-print(food.get("chandana"))
+print(friends)
+print(friends["harshita"])
+print(friends.get("chandana"))
+friends["affren"]="2008"
+print(friends)
+print(friends.keys())
+print(friends.values())
