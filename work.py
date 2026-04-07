8c56a1af1a26900a479a7d920119257c19425ea9
kavana yelshetty <kacy29062007@gmail.com>
2026-04-07 21:22:20 +0530

    file updated

--- work.py
+++ work.py
@@ -3,4 +3,9 @@ for i in range(1,101):
         continue
     if i%3==0 or i%5==0:
         print(i,end=" ")
-
+print(" ")
+t=20
+if t==20:
+    print("its time for dinner")
+else:
+    print("its not a time for dinner")
