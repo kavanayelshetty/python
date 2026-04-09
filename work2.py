5ebe2b0414c9301291e4327439dc4b25f4139343
kavana yelshetty <kacy29062007@gmail.com>
2026-04-09 22:36:38 +0530

    file updated

--- work2.py
+++ work2.py
@@ -23,3 +23,12 @@ standard_deviation=variance**0.5
 print("mean: ",mean)
 print("variance: ",variance)
 print("standard deviation: ",standard_deviation)
+N=int(input("enter the number of terms "))
+first=0
+second=1
+print("fibnocei sequence: ")
+for i in range(N):
+    print(first,end=" ")
+    next_term=first+second
+    first=second
+    second=next_term
