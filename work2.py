47d6af55935175ed9bad528ea93336a9392aa09b
kavana yelshetty <kacy29062007@gmail.com>
2026-04-09 22:30:39 +0530

    file updated

--- work2.py
+++ work2.py
@@ -9,3 +9,17 @@ def letter_find(name_letter):
     return-1
 result=letter_find(letter)
 print("index:",result)
+number_of_values=int(input("enter how many numbers: "))
+number_list=[ ]
+for i in range(number_of_values):
+    number=float(input("enter a number: "))
+    number_list.append(number)
+mean=sum(number_list)/number_of_values
+variance_sum=0
+for number in number_list:
+    variance_sum+=(number-mean)**2
+variance=variance_sum/number_of_values
+standard_deviation=variance**0.5
+print("mean: ",mean)
+print("variance: ",variance)
+print("standard deviation: ",standard_deviation)
