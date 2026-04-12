e6e8eba61a9cc98238259268355e362f6fbff804
kavana yelshetty <kacy29062007@gmail.com>
2026-04-12 22:03:02 +0530

    filr updated

--- work3.py
+++ work3.py
@@ -16,15 +16,9 @@ age=current_year-year_of_birth
 if age>=60:
     print(name,"is a senior citizen")
 else:
-    print(name,"is not a senior citizen")    
-available_seats=2
-while available_seats>0:
-    print(f"{available_seats} seats avalaible")
-    booking=input("do you want to book a seat?(yes/no):").lower()
-    if booking=="yes":
-        available_seats=1
-        print("seat booked!")
-    else:
-        print("no booking made")
-print("all seats are booked !")     
+    print(name,"is not a senior citizen")     
+for i in range(1,6):
+    for j in range(1,6):
+        print(f"{i} x {j} = {i*j}")
+    print()       
 
