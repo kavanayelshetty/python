5d3ffac2ffd8276bfbbbdd819b7539140ea4f96e
kavana yelshetty <kacy29062007@gmail.com>
2026-04-14 21:48:55 +0530

    update filr

---
+++ work4.py
@@ -0,0 +1,9 @@
+# Taking two numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Adding the numbers
+result = num1 + num2
+
+# Displaying the result
+print(f"The sum of {num1} and {num2} is {result}")
