305ef42e680f55efee83501c1269ce7de666d698
kavana yelshetty <kacy29062007@gmail.com>
2026-04-15 23:12:43 +0530

    update file

--- hello.py
+++ hello.py
@@ -13,5 +13,13 @@ print(a*b)
 print(a/b)
 print(a//b)
 print(a%b)
-
+n = int(input("enter the value of N: "))
+a=0
+b=1
+print("fibonacci sequence: ")
+for i in range(n):
+    print(a,end=" ")
+    c=a+b
+    a=b
+    b=c
 
