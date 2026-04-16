c6d930e4e2ae03754160e737a50aa0354c1133c7
kavana yelshetty <kacy29062007@gmail.com>
2026-04-16 22:09:10 +0530

    uodate file

--- work4.py
+++ work4.py
@@ -7,3 +7,11 @@ result = num1 + num2
 
 # Displaying the result
 print(f"The sum of {num1} and {num2} is {result}")
+pin =" "
+correct_pin = "1234"
+while pin!= correct_pin:
+    pin = input("enter your PIN: ")
+    if pin != correct_pin:
+        print("incorrect PIN,try again")
+print("pin accepted you can proceed")        
+
