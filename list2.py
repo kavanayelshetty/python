b42e6ae1ff68bd55ccdb0dcc9989f167f9a2c056
kavana yelshetty <kacy29062007@gmail.com>
2026-04-21 21:23:11 +0530

    file updated

--- list2.py
+++ list2.py
@@ -5,3 +5,8 @@ for friend in friends:
         print(f"{friend} gets a laddu !")
     else:
         print("no laddus left")    
+numbers=[10,20,30,40,50]
+total=0
+for num in numbers:
+    total+=num
+print("total sum: ", total)            
