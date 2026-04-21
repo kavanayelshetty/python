89082b5a070ca3bb4bfae0fa0cee1c88822c19b2
kavana yelshetty <kacy29062007@gmail.com>
2026-04-21 21:18:21 +0530

    file updated

---
+++ list2.py
@@ -0,0 +1,7 @@
+laddus=5
+friends=["rahul","sneha","aman","priya"]
+for friend in friends:
+    if laddus>0:
+        print(f"{friend} gets a laddu !")
+    else:
+        print("no laddus left")    
