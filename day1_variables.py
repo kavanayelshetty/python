bb1b95b794aca13745c2d7720bffb271a7dce80d
kavana yelshetty <kacy29062007@gmail.com>
2026-04-24 21:17:59 +0530

    file updated

--- day1_variables.py
+++ day1_variables.py
@@ -12,3 +12,13 @@ print(x/y)
 print(x//y)
 print(x%y)
 print(x**y)
+numbers=[10,20,30,40,50]
+total=0
+for num in numbers:
+    total=total+num
+print(total)    
+numbers=[1,2,3,4,5,6]
+doubled=[]
+for num in numbers:
+    doubled.append(num*2)
+print("doubled list: ",doubled)
