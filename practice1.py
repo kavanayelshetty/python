6ca5ca82cb55bfac403f031775e57310b400e6a5
kavana yelshetty <kacy29062007@gmail.com>
2026-04-26 21:37:31 +0530

    updated

--- practice1.py
+++ practice1.py
@@ -5,3 +5,11 @@ print(squares)
 numbers=[1,2,3,4,5,6,7,8]
 even_numbers=[num for num in numbers if num%2==0]
 print(even_numbers)
+
+cities=["bengaluru","hubbali","kolar","mysuru"]
+uppercase_cities=[city.upper() for city in cities]
+print(uppercase_cities)
+
+numbers=[1,2,3,4,5,6]
+squares_dict={num:num**2 for num in numbers}
+print(squares)
