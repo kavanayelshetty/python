8e6f5b6ebeab7c3aa119b35ce3c7f1872fc9d036
kavana yelshetty <kacy29062007@gmail.com>
2026-05-03 17:07:47 +0530

    file updated

--- 9.py
+++ 9.py
@@ -3,8 +3,16 @@ class car:
         self.brand=brand
         self.model=model
     def display_info(self):
-        print(f"car brand: {self.brand},model:{self.model}")
+        print(f"car brand: {self.brand} , model: {self.model}")
 
 my_car=car("toyota","corolla")
-my_car.display_info            
+my_car.display_info()            
         
+class person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def greet(self):
+        print(f"hello! my name is  {self.name} ane i am {self.age} years old  ")
+info=person("arun",19)            
+print(info.greet())                
