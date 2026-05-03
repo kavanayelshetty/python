30129de7fd3879203d1630c6166df5691ee3c6e8
kavana yelshetty <kacy29062007@gmail.com>
2026-05-03 16:56:52 +0530

    new file created

---
+++ 9.py
@@ -0,0 +1,10 @@
+class car:
+    def __init__(self,brand,model):
+        self.brand=brand
+        self.model=model
+    def display_info(self):
+        print(f"car brand: {self.brand},model:{self.model}")
+
+my_car=car("toyota","corolla")
+my_car.display_info            
+        
