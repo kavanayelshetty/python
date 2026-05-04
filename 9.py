4754e48561722ca59d34e0082a768b77382b9452
kavana yelshetty <kacy29062007@gmail.com>
2026-05-04 22:05:19 +0530

    updated

--- 9.py
+++ 9.py
@@ -13,6 +13,28 @@ class person:
         self.name=name
         self.age=age
     def greet(self):
-        print(f"hello! my name is  {self.name} ane i am {self.age} years old  ")
+        print(f"hello! my name is  {self.name} and i am {self.age} years old  ")
 info=person("arun",19)            
 print(info.greet())                
+
+class Dog:
+    def __init__(self,name,breed):
+        self.name=name
+        self.breed=breed
+    def bark(self):
+        print(f"{self.name} is barking! ")    
+        
+dog1=Dog("ruby","pemorian")    
+dog2=Dog("bolt","beagle")
+dog1.bark()    
+dog2.bark()    
+
+class person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def introduce(self):
+        print(f"hi! I am {self.name} i am {self.age} years old")
+
+person1=person("anand",20)
+person1.introduce()        
