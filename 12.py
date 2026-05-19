684a88ddc912a1ee40397b6b2e246640aeb3cee6
kavana yelshetty <kacy29062007@gmail.com>
2026-05-19 20:09:41 +0530

    updated

--- 12.py
+++ 12.py
@@ -11,11 +11,24 @@ print("2.substraction")
 print("3.multiplication")
 print("4.division")
 
-choice=int(input("enter choice between 1 - 4"))
+choice=int(input("enter choice between 1 - 4: "))
 
 if choice==1:
     result=num1+num2
     print("result: ",result)
 elif choice==2:
     result=num1-num2
-    print("result: ",result)    
+    print("result: ",result) 
+elif choice==3:
+    result=num1*num2
+    print("result: ",result)
+elif choice==4:
+    if num2!=0:
+        result=num1/num2
+        print("result: ",result)
+    else:
+        print("Error! division by zero is not allowed")
+else:
+    print("invalid input please enter number from 1-4 ")
+
+print("Thank you for using calculator!")            
