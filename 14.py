166da3ddd8517ca33859e4d034a9616f4fbf8d89
kavana yelshetty <kacy29062007@gmail.com>
2026-05-22 17:15:35 +0530

    updated

--- 14.py
+++ 14.py
@@ -19,3 +19,21 @@ if len(password) >= 8 and any(i.isdigit() for i in password):
     print("strong password")
 else:
     print("weak password")    
+
+score =0
+q1=input("capital of india? ") 
+if q1.lower()=="delhi":
+    score+=1
+q2=input("5 x 6 = ")
+
+# Quiz game
+
+if q2=="30":
+    score+=1
+
+q3=input("python is a language? (yes/no): ")
+
+if q3.lower()=="yes":
+    score+=1
+
+print("your score: ",score,"/3")    
