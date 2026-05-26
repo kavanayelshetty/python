b8b71849e525b55cd15d0bab35329007be8dedb7
kavana yelshetty <kacy29062007@gmail.com>
2026-05-26 18:22:14 +0530

    commit

--- 16.py
+++ 16.py
@@ -1,11 +1,21 @@
-#rock paper scissors
+# #rock paper scissors
 
-a=input("choose rock/paper/scissor: ")
-b=input("choose rock/paper/scissor: ")
+# a=input("choose rock/paper/scissor: ")
+# b=input("choose rock/paper/scissor: ")
 
-if a==b:
-    print("draw")
-elif(a=="rock" and b=="scissors") or (a=="paper" and b=="rock") or (a=="scissor" and b=="paper"):
-    print("player 1 wins")
-else:
-    print("player 2 wins")        
+# if a==b:
+#     print("draw")
+# elif(a=="rock" and b=="scissors") or (a=="paper" and b=="rock") or (a=="scissor" and b=="paper"):
+#     print("player 1 wins")
+# else:
+#     print("player 2 wins")  /
+
+
+arr =[5,2,8,1,9]
+
+for i in range(len(arr)):
+    for j in range(len(arr)-1-i):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+
+print("sorted: ",arr)            
