a35a928764ac385655bb3599e612dda009dc7ed1
kavana yelshetty <kacy29062007@gmail.com>
2026-05-31 19:01:17 +0530

    updated

--- 17.py
+++ 17.py
@@ -1,2 +1,10 @@
 for i in range(1,6):
     print("⭐ "*i)
+
+import random
+import string
+
+chars=string.ascii_letters+string.digits+"@#$%"
+
+password="".join(random.choice(chars)for i in range(10))
+print("geneted password: ",password) 
