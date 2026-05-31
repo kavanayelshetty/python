f654d912020770338512b7d1e055f018b1f4168a
kavana yelshetty <kacy29062007@gmail.com>
2026-05-31 19:05:34 +0530

    updated

--- 17.py
+++ 17.py
@@ -8,3 +8,12 @@ chars=string.ascii_letters+string.digits+"@#$%"
 
 password="".join(random.choice(chars)for i in range(10))
 print("geneted password: ",password) 
+
+import time
+
+for i in range(101):
+    bar = "█" * (i//5) + "-" * (20 - i//5)
+    print(f"\r[{bar}] {i}%", end="")
+    time.sleep(0.05)
+
+print("\nUPLOAD COMPLETE 🚀")
