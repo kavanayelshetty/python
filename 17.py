4bfa4ec6e114ce52bc0c91a7c259da1b366946b3
kavana yelshetty <kacy29062007@gmail.com>
2026-06-02 21:45:26 +0530

    uodated

--- 17.py
+++ 17.py
@@ -11,12 +11,12 @@ print("geneted password: ",password)
 
 import time
 
-for i in range(101):
-    bar = "█" * (i//5) + "-" * (20 - i//5)
-    print(f"\r[{bar}] {i}%", end="")
-    time.sleep(0.05)
+# for i in range(101):
+#     bar = "█" * (i//5) + "-" * (20 - i//5)
+#     print(f"\r[{bar}] {i}%", end="")
+#     time.sleep(0.05)
 
-print("\nUPLOAD COMPLETE 🚀")
+# print("\nUPLOAD COMPLETE 🚀")
 
 import random
 
@@ -46,3 +46,6 @@ print(text[::-1])
 
 nums=[5,9,2,14,7]
 print(max(nums))
+
+for i in range(1,6):
+    print("* " * i)
