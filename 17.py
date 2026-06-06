54ef61aff01613b4bcb88cbf723646dd94c8d3d6
kavana yelshetty <kacy29062007@gmail.com>
2026-06-06 20:12:08 +0530

    updated

--- 17.py
+++ 17.py
@@ -120,3 +120,12 @@ try:
     print(10/a)
 except ZeroDivisionError:
     print("you cant divide by zero! ")    
+
+try:
+    num=int(input("enter a number: "))
+    result=10/num
+    print("result: ",result)
+except ZeroDivisionError:
+    print("cannot divide by zero!")
+except ValueError:
+    print("please enter a valid number.")    
