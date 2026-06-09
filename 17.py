50c9ff47584cce57705d942f9b0c144be155644e
kavana yelshetty <kacy29062007@gmail.com>
2026-06-09 17:20:11 +0530

    updated

--- 17.py
+++ 17.py
@@ -153,5 +153,17 @@ count=0
 for letter in word:
     if letter in vowels:
         count+=1
-print("number of vowels: ",count)        
+print("number of vowels: ",count)   
+
+# Find the largest number in a list
+
+numbers = [12, 45, 7, 89, 23]
+
+largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        largest = num
+
+print("Largest number is:", largest)
 
