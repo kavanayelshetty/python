c239ae6588be9c69f4ab2ec2a1bbecdd4d295541
kavana yelshetty <kacy29062007@gmail.com>
2026-06-12 21:52:09 +0530

    updated

--- 19.py
+++ 19.py
@@ -9,3 +9,13 @@ if num > 1:
         print("Prime")
 else:
     print("Not Prime")
+
+text = "Python Programming"
+
+count = 0
+
+for ch in text.lower():
+    if ch in "aeiou":
+        count += 1
+
+print("Number of vowels:", count)    
