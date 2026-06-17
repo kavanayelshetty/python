c3eade5c6a41ed44396ba9f44df2df801b5af976
kavana yelshetty <kacy29062007@gmail.com>
2026-06-17 19:47:56 +0530

    updated

--- 19.py
+++ 19.py
@@ -5,3 +5,9 @@ b = 20
 a, b = b, a
 print("a =", a)
 print("b =", b)
+
+# Swap first and last element of a list
+
+numbers = [10, 20, 30, 40, 50]
+numbers[0], numbers[-1] = numbers[-1], numbers[0]
+print(numbers)
