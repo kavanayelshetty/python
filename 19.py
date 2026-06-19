700e95aba1901678cbee56a704230140ea586eb3
kavana yelshetty <kacy29062007@gmail.com>
2026-06-19 21:41:37 +0530

    updated

--- 19.py
+++ 19.py
@@ -33,3 +33,9 @@ else:
 num = 13
 binary = bin(num)
 print("Binary:", binary)      
+
+# Print numbers from 1 to 20 that are divisible by 3
+
+for i in range(1, 21):
+    if i % 3 == 0:
+        print(i)
