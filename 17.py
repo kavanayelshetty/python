111901a407f54aacdb37e888d7579208205ad2e7
kavana yelshetty <kacy29062007@gmail.com>
2026-06-23 19:45:28 +0530

    updated

--- 17.py
+++ 17.py
@@ -1,14 +1,27 @@
+# import random
+
+# secret = random.randint(1, 100)
+
+# while True:
+#     guess = int(input("Guess a number between 1 and 100: "))
+
+#     if guess < secret:
+#         print("Too low!")
+#     elif guess > secret:
+#         print("Too high!")
+#     else:
+#         print("Congratulations! You guessed it.")
+#         break
+
 import random
 
-secret = random.randint(1, 100)
+quotes = [
+    "Success comes from consistency.",
+    "Small progress is still progress.",
+    "Discipline beats motivation.",
+    "Keep learning every day.",
+    "Focus on the process."
+]
 
-while True:
-    guess = int(input("Guess a number between 1 and 100: "))
-
-    if guess < secret:
-        print("Too low!")
-    elif guess > secret:
-        print("Too high!")
-    else:
-        print("Congratulations! You guessed it.")
-        break
+print("Quote of the Day:")
+print(random.choice(quotes))
