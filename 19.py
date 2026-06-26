06e668dbb465190bde4cf9c9a0418cec17404ec1
kavana yelshetty <kacy29062007@gmail.com>
2026-06-26 20:52:29 +0530

    updated

--- 19.py
+++ 19.py
@@ -6,3 +6,10 @@ if text == text[::-1]:
     print("The string is a palindrome.")
 else:
     print("The string is not a palindrome.")
+
+list1 = [10, 20, 30]
+
+list2 = list1 + [40]
+
+print("List1:", list1)
+print("List2:", list2)
