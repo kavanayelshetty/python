622fa415b0a8d6a634effb746da836184b891fec
kavana yelshetty <kacy29062007@gmail.com>
2026-06-29 22:22:27 +0530

    updated

--- 15.py
+++ 15.py
@@ -31,3 +31,15 @@ for num in numbers:
         largest = num
 
 print("Largest number:", largest)
+
+# Count vowels in a string
+
+text = "Hello Python"
+
+count = 0
+
+for ch in text.lower():
+    if ch in "aeiou":
+        count += 1
+
+print("Number of vowels:", count)
