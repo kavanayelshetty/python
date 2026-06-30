32a8bc7ed49ba38e0c56a0465994ef816f836c06
kavana yelshetty <kacy29062007@gmail.com>
2026-06-30 19:34:52 +0530

    updated

--- 15.py
+++ 15.py
@@ -9,3 +9,12 @@ if sum == num:
     print(num, "is a Perfect Number")
 else:
     print(num, "is not a Perfect Number")
+
+text = "python programming"
+count = 0
+
+for ch in text:
+    if ch in "aeiouAEIOU":
+        count += 1
+
+print("Number of vowels:", count)    
