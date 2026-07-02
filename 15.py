02e5b50faa2ee86d65d4389343e4bc35a2cc22ed
kavana yelshetty <kacy29062007@gmail.com>
2026-07-02 20:10:08 +0530

    updated

--- 15.py
+++ 15.py
@@ -45,3 +45,16 @@ for i in numbers:
     print(i, "appears", numbers.count(i), "times")
     while i in numbers:
         numbers.remove(i)
+
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+merged = []
+
+for i in list1:
+    merged.append(i)
+
+for i in list2:
+    merged.append(i)
+
+print("Merged List:", merged)        
