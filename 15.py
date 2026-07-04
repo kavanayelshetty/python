ff0d3989672de26e07de830f8735d475707dfe5d
kavana yelshetty <kacy29062007@gmail.com>
2026-07-04 12:47:13 +0530

    updated

--- 15.py
+++ 15.py
@@ -8,3 +8,16 @@ elif b >= a and b >= c:
     print("Largest number is:", b)
 else:
     print("Largest number is:", c)
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+n = len(arr)
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted array:")
+print(arr)    
