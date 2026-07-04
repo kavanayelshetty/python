1b43602a9a66582eeaf7cc2eaba795d07ddac54a
kavana yelshetty <kacy29062007@gmail.com>
2026-07-04 12:45:28 +0530

    updated

---
+++ 15.py
@@ -0,0 +1,10 @@
+a = 15
+b = 28
+c = 21
+
+if a >= b and a >= c:
+    print("Largest number is:", a)
+elif b >= a and b >= c:
+    print("Largest number is:", b)
+else:
+    print("Largest number is:", c)
