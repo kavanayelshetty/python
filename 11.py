cc6529841be5d9338ffa7bc7e039062abe41849d
kavana yelshetty <kacy29062007@gmail.com>
2026-07-18 21:49:52 +0530

    updated

--- 11.py
+++
@@ -1,9 +0,0 @@
-numbers = [12, 45, 7, 89, 23, 89, 56]
-
-unique_numbers = list(set(numbers))
-unique_numbers.sort()
-
-if len(unique_numbers) >= 2:
-    print("Second largest number:", unique_numbers[-2])
-else:
-    print("No second largest number exists.")
