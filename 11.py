4fdc0b8157b4c40094101cf1ba39500f20b1ccd4
Kavana Yelshetty <kacy29062007@gmail.com>
2026-07-19 23:47:58 +0530

    Update 11.py

---
+++ 11.py
@@ -0,0 +1,14 @@
+num = 153
+temp = num
+sum = 0
+digits = len(str(num))
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** digits
+    temp //= 10
+
+if sum == num:
+    print(num, "is an Armstrong number")
+else:
+    print(num, "is not an Armstrong number")
