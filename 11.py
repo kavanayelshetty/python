dbad2ccdbfc176f2020da79d8f67f6b86ef8f026
kavana yelshetty <kacy29062007@gmail.com>
2026-07-27 21:02:36 +0530

    updated

--- 11.py
+++ 11.py
@@ -8,3 +8,13 @@ def welcome(func):
 def intro():
     print("i am kavana")
 intro()
+
+numbers = [12, 45, 7, 89, 34, 56]
+
+largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        largest = num
+
+print("Largest number:", largest)
