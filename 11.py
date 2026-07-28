40097262b4e1ec59f031d1d4dc9a1d530fe8047f
kavana yelshetty <kacy29062007@gmail.com>
2026-07-28 15:04:04 +0530

    updated

--- 11.py
+++ 11.py
@@ -1,11 +1,1 @@
-def welcome(func):
-    def wrapper():
-        print("namaskara")
-        func()
-        print("take care!")
-    return wrapper    
-@welcome
-def intro():
-    print("i am kavana")
-intro()
 
