e86c3933465679bdae272bf45ae592a2709dbc69
kavana yelshetty <kacy29062007@gmail.com>
2026-07-31 15:44:46 +0530

    updated

--- dic.py
+++ dic.py
@@ -5,4 +5,9 @@ print(var)
 var2 = np.array([1,2,3])
 for i in np.nditer(var2,op_flags=['readwrite']):
     i[...]=i*2
-print(var2)    
+print(var2)
+
+arr = np.array([[1,2],[2,4]])
+it=np.nditer(arr,flags=['multi_index'])
+for x in it:
+    print(it.multi_index,x)    
