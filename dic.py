2e40fbccdb3f326db3d6752a45100f295a467dbb
kavana yelshetty <kacy29062007@gmail.com>
2026-07-31 15:21:01 +0530

    updated

--- dic.py
+++ dic.py
@@ -2,3 +2,7 @@ import numpy as np
 var=np.array([[[1,2,3],[0,9,6],[3,9,5],[7,6,1]]])
 print(var)
 
+var2 = np.array([1,2,3])
+for i in np.nditer(var2,op_flags=['readwrite']):
+    i[...]=i*2
+print(var2)    
