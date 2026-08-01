4641e8affc10fe3dd76a215045ced3dc91bf4aa5
kavana yelshetty <kacy29062007@gmail.com>
2026-08-01 20:13:41 +0530

    updated

--- 11.py
+++ 11.py
@@ -17,3 +17,8 @@ print(np.array([[[1,4,5,7],[5,6,74,8]]]))
 arr_2=np.array([1,2,3,4])
 print(np.cos(arr_2))
 print(np.cumsum(arr_2))
+
+import numpy as np
+v1 = np.array([1,2,3,4])
+v=np.insert(v1,(2,4),60)
+print(var,v)
