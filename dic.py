01fedc577113c368e138332520e95aa09e60c8a3
kavana yelshetty <kacy29062007@gmail.com>
2026-08-03 22:11:46 +0530

    updated

--- dic.py
+++ dic.py
@@ -40,14 +40,3 @@ for ch in text:
 
 print("Number of vowels:", count)    
 
-n = int(input("Enter the number of terms: "))
-
-a = 0
-b = 1
-
-print("Fibonacci Series:")
-for i in range(n):
-    print(a, end=" ")
-    c = a + b
-    a = b
-    b = c
