6bc5dd0f58a4f3030ea57cfc51d7a22a30b8d02f
kavana yelshetty <kacy29062007@gmail.com>
2026-08-10 22:17:03 +0530

    updated

--- 11.py
+++ 11.py
@@ -28,3 +28,13 @@ v3=np.insert(v2,(2,4),19)
 print(v3)
 print(v2)
 
+text = "Python Programming"
+vowels = "aeiou"
+
+count = 0
+
+for char in text.lower():
+    if char in vowels:
+        count += 1
+
+print("Number of vowels:", count)
