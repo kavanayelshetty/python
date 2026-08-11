be986f9ba84c6d4c0652a7f0ca93eba34a40b7ff
kavana yelshetty <kacy29062007@gmail.com>
2026-08-11 23:00:39 +0530

    updated

--- 11.py
+++ 11.py
@@ -32,12 +32,3 @@ print(v2)
 
 text = "python is easy and python is powerful"
 
-words = text.split()
-frequency = {}
-
-for word in words:
-    frequency[word] = frequency.get(word, 0) + 1
-
-print("Word Frequency:")
-for word, count in frequency.items():
-    print(word, ":", count)
