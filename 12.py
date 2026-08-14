885665941fed9cdf5c718cff61d5205f90a70074
kavana yelshetty <kacy29062007@gmail.com>
2026-08-14 21:44:18 +0530

    updated

---
+++ 12.py
@@ -0,0 +1,11 @@
+num = 17
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
+else:
+    print("Not a prime number")
