252a059c0e8720b3f4b219e689ead115d0910140
kavana yelshetty <kacy29062007@gmail.com>
2026-08-20 21:29:30 +0530

    updated

--- 13.py
+++ 13.py
@@ -16,10 +16,3 @@ var2=var.to_csv("text.csv",index=False)
 
 var1=pd.read_csv("C:\\Users\\Kavana\\OneDrive\\Desktop\\python project\\text.csv",skiprows=[1])
 print(var1)
-
-num = 17
-
-if num % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
