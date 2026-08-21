4aa35ef7bd649c5b7e21ac001d30b73d9d8731ca
kavana yelshetty <kacy29062007@gmail.com>
2026-08-21 22:09:49 +0530

    updated

--- 13.py
+++ 13.py
@@ -17,9 +17,3 @@ var2=var.to_csv("text.csv",index=False)
 var1=pd.read_csv("C:\\Users\\Kavana\\OneDrive\\Desktop\\python project\\text.csv",skiprows=[1])
 print(var1)
 
-text = "Python"
-
-reverse = text[::-1]
-
-print("Original:", text)
-print("Reverse:", reverse)
