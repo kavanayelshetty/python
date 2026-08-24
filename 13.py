826ac2fc3416e02fb54c1946d01745bd65e90e2f
kavana yelshetty <kacy29062007@gmail.com>
2026-08-24 22:23:17 +0530

    updated

--- 13.py
+++ 13.py
@@ -17,3 +17,14 @@ var2=var.to_csv("text.csv",index=False)
 var1=pd.read_csv("C:\\Users\\Kavana\\OneDrive\\Desktop\\python project\\text.csv",skiprows=[1])
 print(var1)
 
+num = int(input("Enter a number: "))
+
+if num < 2:
+    print("Not a prime number")
+else:
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
