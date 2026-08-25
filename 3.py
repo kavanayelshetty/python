8d5ae74c691e239779629ef99a6c571cc79fec43
kavana yelshetty <kacy29062007@gmail.com>
2026-08-25 19:51:48 +0530

    updated

--- 3.py
+++ 3.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(1, 100)
 
 while True:
-    guess = int(input("Guess a number between 1 and 100: "))
+    guess = int(input("Guess a number between 1 and 10: "))
 
     if guess < number:
         print("Too low!")
