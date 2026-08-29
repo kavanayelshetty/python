786ce6a2366b06320ffde9c8aaf5f5ba0eebdeef
kavana yelshetty <kacy29062007@gmail.com>
2026-08-29 22:09:00 +0530

    updated

--- 13.py
+++ 13.py
@@ -17,3 +17,9 @@ var2=var.to_csv("text.csv",index=False)
 var1=pd.read_csv("C:\\Users\\Kavana\\OneDrive\\Desktop\\python project\\text.csv",skiprows=[1])
 print(var1)
 
+password = input("Enter password: ")
+
+if password == "python123":
+    print("Access Granted")
+else:
+    print("Wrong Password")
