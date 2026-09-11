7805b77e6f31c42b8b28d102c8d7111cd8baeb82
kavana yelshetty <kacy29062007@gmail.com>
2026-09-11 21:55:20 +0530

    updated

--- 13.py
+++ 13.py
@@ -33,10 +33,3 @@ print("Largest number:", largest)
 
 text = "Python Programming"
 
-count = 0
-
-for char in text.lower():
-    if char in "aeiou":
-        count += 1
-
-print("Number of vowels:", count)
