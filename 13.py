b5b148d886dbf16b8de991855092e5fc2d0302ad
kavana yelshetty <kacy29062007@gmail.com>
2026-09-14 22:51:37 +0530

    updated

--- 13.py
+++ 13.py
@@ -17,14 +17,3 @@ var2=var.to_csv("text.csv",index=False)
 var1=pd.read_csv("C:\\Users\\Kavana\\OneDrive\\Desktop\\python project\\text.csv",skiprows=[1])
 print(var1)
 
-num = 5
-factorial = 1
-
-for i in range(1, num + 1):
-    factorial *= i
-
-print("Factorial:", factorial)
-
-numbers = [12, 45, 7, 89, 34]
-
-print("Largest number is:", max(numbers))
