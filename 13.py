21646e149e9014ed609b2d05e76a7622c3da51e8
kavana yelshetty <kacy29062007@gmail.com>
2026-09-17 22:08:41 +0530

    updated

--- 13.py
+++ 13.py
@@ -18,12 +18,3 @@ var1=pd.read_csv("C:\\Users\\Kavana\\OneDrive\\Desktop\\python project\\text.csv
 print(var1)
 
 
-numbers = [12, 45, 7, 89, 23]
-
-largest = numbers[0]
-
-for num in numbers:
-    if num > largest:
-        largest = num
-
-print("Largest number:", largest)
