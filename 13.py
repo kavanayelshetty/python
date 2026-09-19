1657ef59ea20a96fde85f4df9a53fce8c2364a45
kavana yelshetty <kacy29062007@gmail.com>
2026-09-19 17:17:57 +0530

    updated

--- 13.py
+++ 13.py
@@ -29,10 +29,3 @@ print("Number of vowels:", count)
 
 numbers = [12, 45, 7, 89, 34]
 
-largest = numbers[0]
-
-for num in numbers:
-    if num > largest:
-        largest = num
-
-print("Largest number:", largest)
