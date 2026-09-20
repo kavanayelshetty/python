edf888b347a65167533bb5838112634363549e7b
kavana yelshetty <kacy29062007@gmail.com>
2026-09-20 23:08:34 +0530

    updated

--- 13.py
+++ 13.py
@@ -17,3 +17,12 @@ var2=var.to_csv("text.csv",index=False)
 var1=pd.read_csv("C:\\Users\\Kavana\\OneDrive\\Desktop\\python project\\text.csv",skiprows=[1])
 print(var1)
 
+text = "Hello World"
+vowels = "aeiou"
+
+count = 0
+for ch in text.lower():
+    if ch in vowels:
+        count += 1
+
+print("Number of vowels:", count)
