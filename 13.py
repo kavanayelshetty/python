1f2b864a01da214a0b951306d2943c99c7baacb6
kavana yelshetty <kacy29062007@gmail.com>
2026-09-24 21:49:54 +0530

    updated

--- 13.py
+++ 13.py
@@ -43,3 +43,12 @@ for num in numbers:
 
 print("Largest number:", largest)
 
+numbers = [12, 45, 23, 67, 34]
+
+largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        largest = num
+
+print("Largest number:", largest)
