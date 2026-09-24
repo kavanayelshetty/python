341d551a9595c6812bbe1eeee08ea5abe1a2ce0f
kavana yelshetty <kacy29062007@gmail.com>
2026-09-24 21:48:08 +0530

    updated

--- 13.py
+++ 13.py
@@ -43,3 +43,12 @@ for num in numbers:
 
 print("Largest number:", largest)
 
+text = "Artificial Intelligence"
+vowels = "aeiou"
+count = 0
+
+for ch in text.lower():
+    if ch in vowels:
+        count += 1
+
+print("Number of vowels:", count)
