0d0afde3323985836951338397d16e39810c8a73
kavana yelshetty <kacy29062007@gmail.com>
2026-09-25 19:47:50 +0530

    updated

--- 13.py
+++ 13.py
@@ -33,3 +33,12 @@ for ch in text:
 
 print("Number of vowels:", count)    
 
+num = 12345
+total = 0
+
+while num > 0:
+    digit = num % 10
+    total += digit
+    num //= 10
+
+print("Sum of digits:", total)
